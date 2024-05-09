27a82c28edd9a84a1e9af463a2da50b0114ab75b
Nox-Devlopment <noxdevlopment@gmail.com>
2024-05-09 23:18:30 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -33,11 +33,11 @@ export default function Partners() {
 
 
                             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me/partners/64db583a4292ca1066ad0a1b">
+            <a href="https://discord.gg/JEF4M4uG5e">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1172657853056684102/1172671829673005146/maskot_profil_alev.png" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Nodesty VDS</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Nox Devlopment</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
                                    
